ceaece860cfcc5a849a1c47f2d6b833e978482c1
iryndin <iryndin@gmail.com>
2013-04-18 18:35:10 +0400

    making watchdog demo

---
+++ actorDemos/src/main/java/net/iryndin/akka/examples/actordemos/ActorWatchdogDemo.java
@@ -0,0 +1,120 @@
+package net.iryndin.akka.examples.actordemos;
+
+import akka.actor.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Demonstrates creation of actor with UntypedActorFactory
+ * Will fetch Google PageRank for a domain
+ */
+public class ActorWatchdogDemo {
+
+    public static void main( String[] args ) throws Exception {
+        final ActorSystem actorSystem = ActorSystem.create("as1");
+        final ActorRef actor = actorSystem.actorOf(new Props(WatchDogActor.class));
+        actor.tell("start", null);
+        Thread.sleep(10_000);
+        actor.tell("killall", null);
+        Thread.sleep(2000);
+        //actor.tell(PoisonPill.getInstance(), null);
+        //actorSystem.shutdown();
+
+    }
+
+    /**
+     * WatchDog Actor which watches all child actors and gets Terminated message from them
+     */
+    static class WatchDogActor extends UntypedActor {
+
+        final int N = 10;
+        final String actorName = "WatchDogActor";
+
+        final ActorRef[] watchedActors = new ActorRef[N];
+
+        public WatchDogActor() {
+            createWatchedActors();
+        }
+
+        private void createWatchedActors() {
+            for (int i=0; i<N; i++) {
+                final int j = i;
+                watchedActors[i] = getContext().actorOf(new Props(new UntypedActorFactory() {
+                    @Override
+                    public Actor create() throws Exception {
+                        return new WorkerActor(j);
+                    }
+                }), "worker"+j);
+                getContext().watch(watchedActors[i]);
+            }
+        }
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if (message.equals("start")) {
+                sendAllChildren("start");
+            } else if (message.equals("killall")) {
+                for (int i=0; i<N; i++) {
+                    getContext().stop(watchedActors[i]);
+                }
+            } else {
+                System.out.println("Watchdog get: " + message);
+                unhandled(message);
+            }
+        }
+
+        private void sendAllChildren(String s) {
+            for (int i=0; i<N; i++) {
+                watchedActors[i].tell(s, getSelf());
+            }
+        }
+
+        @Override
+        public void preStart() {
+            System.out.println(actorName + " started");
+        }
+
+        @Override
+        public void postStop() {
+            System.out.println(actorName + " stopped");
+        }
+    }
+
+    static class WorkerActor extends UntypedActor {
+
+        private final int number;
+
+        public WorkerActor(final int number) {
+            this.number = number;
+        }
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if (message.equals("start")) {
+                calculatePrimeNumbers();
+            } else {
+                unhandled(message);
+            }
+        }
+
+        private void calculatePrimeNumbers() throws InterruptedException {
+            for (int i=0; i<Integer.MAX_VALUE; i++) {
+                double k = i/23213.123213;
+            }
+        }
+
+        @Override
+        public void preStart() {
+            //System.out.println("Start Worker " + number);
+        }
+
+        @Override
+        public void postStop() {
+            //System.out.println("Stop Worker " + number);
+        }
+    }
+}
